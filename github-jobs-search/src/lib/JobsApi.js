210d5fb1bbe38fb8deead5486f5e65ea0aa6be9c
sera <serafinimatias2@gmail.com>
2018-07-16 12:17:17 -0300

    search component | problem with ajax call |

---
+++ github-jobs-search/src/lib/JobsApi.js
@@ -0,0 +1,37 @@
+//Import Axios for Api call
+import Axios from 'axios';
+
+
+const xhr = new XMLHttpRequest();
+
+const ChangeUrl = (params) => {
+  let AddUrl = 'https://jobs.github.com/positions.json?callback=Request';
+  if (params.location && params.description) {
+    AddUrl += 'description=' + params.description;
+    AddUrl += 'location=' + params.location;
+  } else if (params.location) {
+    AddUrl += 'location=' + params.location;
+  } else if (params.description) {
+    AddUrl += 'description=' + params.description;
+  }
+  return AddUrl;
+}
+
+const Request = (method,params) => {
+  let url = ChangeUrl(params)
+  let promise = new Promise((resolve,reject) => {
+      Axios.get(url)
+      xhr.open(method,url,true);
+      xhr.send();
+      xhr.onload = () => {
+        if(xhr.readyState == 4 && xhr.status == 200){
+          resolve(JSON.parse(xhr.responseText));
+        }else{
+          reject("Error");
+        }
+      }
+  })
+  return promise;
+}
+
+export default Request;

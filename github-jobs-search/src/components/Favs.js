0fd0ab95ec3dfacdb48c83fe368bd2b09288a0af
Julian Estefan <julian.y.estefan@gmail.com>
2018-07-18 10:03:37 -0300

    Added prop favorites to fav component to send the data about favorites to the component. And created the links inside to render them.

--- github-jobs-search/src/components/Favs.js
+++ github-jobs-search/src/components/Favs.js
@@ -6,11 +6,11 @@ class Favs extends Component {
       <div className="favs-container">
         <div className="individual">
           <div className="ind-header grid-container">
-            <a href="#" className="job-title">Job Title</a>
-            <span className="locate">Location</span>
+            <a href="#" className="job-title"> {this.props.favorites.tittle} </a>
+            <span className="locate"> {this.props.favorites.location} </span>
           </div>
           <div className="ind-footer">
-            <span className="info">Company - FullTime</span>
+            <span className="info"> {this.props.favorites.company + "-" + this.props.favorites.type}</span>
             <span class="icon-heart nofav"></span>
           </div>
         </div>
